c868bb737299e68cf8bef335c3c56ab3dedd7b78
Leonardo Rodrigues <lrm5060@gmail.com>
2022-05-30 00:25:05 -0300

    - Add dependecies to package.json - Add assets and css to project - Implement html files to pop-up in screnn - Implement backend in nodejs

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-import { Express } from "express";
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const users = [];
+
+const app = express();
+
+app
+    .use(express.static('public'))
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+
+    .post('/profile', (req, res) => {
+
+        console.log(users);
+        var email = req.body.email;
+        var password = req.body.password;
+        
+        var user = users.find( user => {
+            return user.email === email;
+        });
+
+        if (user !== undefined && user.password === password){
+            return res.redirect(`/profile/?email=${user.email}&name=${user.name}`);
+
+        }
+    })
+
+    .get('/signup', (req, res) => {
+        console.log(req);
+        res.sendFile('public/signUp.html', { root: __dirname });
+    })
+
+    .get('/profile', (req, res) => {
+        res.sendFile('public/profile.html', { root: __dirname });
+    })
+
+    .post('/', (req, res) => {
+        var user = {
+            email: req.body.email,
+            name: req.body.name,
+            password: req.body.password
+        };
+        users.push(user);
+        console.log(users);
+        return res.redirect('/');
+    })
+
+    .listen(3000);
