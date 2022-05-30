f5b9dfa556e54f172eecce5c31cba5fa005ab27c
Leonardo Rodrigues <lrm5060@gmail.com>
2022-05-30 01:34:42 -0300

    Add hash password in memory

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
+import bcrypt from 'bcrypt';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const users = [];
+const userLogged = null;
 
 const app = express();
 
@@ -15,20 +17,25 @@ app
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
 
-    .post('/profile', (req, res) => {
+    .post('/profile', async (req, res) => {
 
         console.log(users);
         var email = req.body.email;
         var password = req.body.password;
         
-        var user = users.find( user => {
+        const user = users.find( user => {
             return user.email === email;
         });
 
-        if (user !== undefined && user.password === password){
-            return res.redirect(`/profile/?email=${user.email}&name=${user.name}`);
-
+        if ( user == null){
+            return;
         }
+        try {
+            if (await bcrypt.compare(password, user.password)){
+                return res.redirect(`/profile/?email=${user.email}&name=${user.name}`);
+                userLogged = user;
+            }
+        } catch { return; }
     })
 
     .get('/signup', (req, res) => {
@@ -40,14 +47,26 @@ app
         res.sendFile('public/profile.html', { root: __dirname });
     })
 
-    .post('/', (req, res) => {
-        var user = {
-            email: req.body.email,
-            name: req.body.name,
-            password: req.body.password
-        };
-        users.push(user);
-        console.log(users);
+    .get('/', (req, res) => {
+        userLogged = null;
+        res.sendFile('public/index.html', { root: __dirname})
+    })
+
+    .post('/', async (req, res) => {
+
+        try {
+            const salt = await bcrypt.genSalt();
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            var user = {
+                email: req.body.email,
+                name: req.body.name,
+                password: hashedPassword
+            };
+            users.push(user);
+            console.log(users);
+        } catch {
+            res.status(500).send();
+        }
         return res.redirect('/');
     })
 
